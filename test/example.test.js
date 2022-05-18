cd66f40e7cc1f53c5cbad28dda9478042f6f7620
Chad Stabler <stablerpsn@gmail.com>
2022-05-18 14:30:36 -0500

    bug fixing test issues

--- test/example.test.js
+++ test/example.test.js
@@ -2,18 +2,16 @@
 // import { example } from '../example.js';
 
 import { renderData } from '../render-utils.js';
-import { fetchPosts } from '../fetch-utils.js';
 
 const test = QUnit.test;
 
-test('renderData for creating a div and populating it with data', async (expect) => {
+test('renderData for creating a div and populating it with data', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const data = await fetchPosts();
     const expected = '<div class="sticky"><h2>Test data</h2><p>This is to test that my data is displaying on the home page correctly</p><h3>Developer</h3></div>';
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = renderData(data[0]);
+    const actual = renderData({ title: 'Test data', content: 'This is to test that my data is displaying on the home page correctly', contact: 'Developer' });
 
     //Expect
     // Make assertions about what is expected versus the actual result
